206a2c6c4465a07dd1bd35e413e73d11c6c5e08c
Abinav Narayanan <53429807+abinavnarayanan18@users.noreply.github.com>
2025-09-17 14:07:15 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: "#0a0a0a",
+        red: "#b30000",
+        white: "#ffffff"
+      }
+    },
+  },
+  plugins: [],
+}
